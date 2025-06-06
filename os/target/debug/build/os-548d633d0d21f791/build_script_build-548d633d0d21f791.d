4105c802d1a321f24bb36a57f7b1167b91fa6cc8
Chaoyi Huang <137665332+XsystemH@users.noreply.github.com>
2025-06-06 23:32:37 +0800

    Delete os/target directory

--- os/target/debug/build/os-548d633d0d21f791/build_script_build-548d633d0d21f791.d
+++
@@ -1,5 +0,0 @@
-/home/xhsystem/Rust/rCore/os/target/debug/build/os-548d633d0d21f791/build_script_build-548d633d0d21f791: build.rs
-
-/home/xhsystem/Rust/rCore/os/target/debug/build/os-548d633d0d21f791/build_script_build-548d633d0d21f791.d: build.rs
-
-build.rs:
--- os/target/debug/deps/riscv_target-830bea0f03bdd7e5.d
+++
@@ -1,7 +0,0 @@
-/home/xhsystem/Rust/rCore/os/target/debug/deps/libriscv_target-830bea0f03bdd7e5.rmeta: /home/xhsystem/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/riscv-target-0.1.2/src/lib.rs
-
-/home/xhsystem/Rust/rCore/os/target/debug/deps/libriscv_target-830bea0f03bdd7e5.rlib: /home/xhsystem/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/riscv-target-0.1.2/src/lib.rs
-
-/home/xhsystem/Rust/rCore/os/target/debug/deps/riscv_target-830bea0f03bdd7e5.d: /home/xhsystem/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/riscv-target-0.1.2/src/lib.rs
-
-/home/xhsystem/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/riscv-target-0.1.2/src/lib.rs:
